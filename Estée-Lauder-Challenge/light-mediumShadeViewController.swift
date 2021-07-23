e8b6178b5c11c73544718a1c6f43406ffc678ea0
Sequoia Taylor <sequoiataylor123@gmail.com>
2021-07-22 18:56:17 -0700

    Estee lauder challenge

---
+++ Estée-Lauder-Challenge/light-mediumShadeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  light-mediumShadeViewController.swift
+//  Estée-Lauder-Challenge
+//
+//  Created by Sequoia Taylor on 7/21/21.
+//
+
+import UIKit
+
+class light_mediumShadeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func lightmediumOrder(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.esteelauder.com/product/643/22830/product-catalog/makeup/face/foundation/double-wear/stay-in-place-foundation?shade=1W1_Bone")! as URL, options: [:], completionHandler: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
